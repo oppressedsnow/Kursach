24bab75ca8c08256c118951385d8deaadeae46cf
snow <44925043+oppressedsnow@users.noreply.github.com>
2023-06-07 14:22:51 +0300

    added score

--- test/core/src/com/mygdx/game/MyGdxGame.java
+++ test/core/src/com/mygdx/game/MyGdxGame.java
@@ -5,26 +5,28 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.audio.Music;
 import com.badlogic.gdx.audio.Sound;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.utils.Array;
 import com.badlogic.gdx.utils.ScreenUtils;
 import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.utils.StringBuilder;
 import com.badlogic.gdx.utils.TimeUtils;
 import java.util.Iterator;
 
 
 public class MyGdxGame extends ApplicationAdapter {
+
 	private OrthographicCamera camera;
 	private Rectangle MainCharacter;
 	private Rectangle GroundRectangle;
 	private Array<Rectangle> raindrops;
 	private long lastDropTime;
-
-	float MCWidth = 100;
-	float MCHeight = 128;
 	float prevx;
 	float prevy;
 	private Texture MCImage;
@@ -32,13 +34,14 @@ public class MyGdxGame extends ApplicationAdapter {
 	private Texture Coin;
 	private Sound CoinPick;
 	private Music forest;
-
-
+	private BitmapFont font;
 	SpriteBatch batch;
 	Texture background;
 	private float VelocityY; // текущая вертикальная скорость объекта
 	private boolean isJumping = false; // флаг, указывающий, прыгает ли объект
 	private static final float Gravity = -600f; // ускорение свободного падения в м/с^2 (влияет на висоту прыжка я хз)
+	int ScoreNum;
+	String YourScoreNum;
 
 	@Override
 	public void create () {
@@ -49,28 +52,35 @@ public class MyGdxGame extends ApplicationAdapter {
 		batch = new SpriteBatch();
 		MCImage = new Texture(Gdx.files.internal("idle.png")); //картінка нашого перса
 		background = new Texture(Gdx.files.internal("bg0.png")); // картинка 1920х1080 (все працює чотко)
-		MainCharacter = new Rectangle(MCWidth, MCHeight, MCImage.getWidth(), MCImage.getHeight());
-		MainCharacter.x = 100; //положення персонажа по х
-		MainCharacter.y = 500; //положення персонажа по у
+		MainCharacter = new Rectangle(84, 256, MCImage.getWidth(), MCImage.getHeight());
+		MainCharacter.x = 400; //положення персонажа по х
+		MainCharacter.y = 400; //положення персонажа по у
 		Ground = new Texture(Gdx.files.internal("Ground.png"));
 		GroundRectangle = new Rectangle(64, 64, Ground.getWidth(), Ground.getHeight());
-		GroundRectangle.x = 100; //положення об'єкта по х
-		GroundRectangle.y = 0; //положення об'єкта по у
+		GroundRectangle.x = 960; //положення об'єкта по х
+		GroundRectangle.y = 120; //положення об'єкта по у
 		prevx = 0;
 		prevy = 0;
-		Coin = new Texture(Gdx.files.internal("coin.png"));
+
 		CoinPick = Gdx.audio.newSound(Gdx.files.internal("CoinPick.mp3"));
 		forest = Gdx.audio.newMusic(Gdx.files.internal("forest.mp3"));
 		forest.setLooping(true);
 		forest.play();
+
+		font = new BitmapFont();
+		font.setColor(Color.RED);
+		ScoreNum = 0;
+		YourScoreNum = "score: 0";
 	}
 
+
 	private void spawnRaindrop() {
-		Rectangle coin = new Rectangle();
-		coin.x = MathUtils.random(0, 800-64);
-		coin.y = 480;
-		coin.width = 64;
-		coin.height = 64;
+		Coin = new Texture(Gdx.files.internal("coin.png"));
+		Rectangle coin = new Rectangle(64, 64, Coin.getWidth(), Coin.getHeight());
+		coin.x = MathUtils.random(200, 1600);
+		coin.y = 900;
+		coin.width = 32;
+		coin.height = 32;
 		raindrops.add(coin);
 		lastDropTime = TimeUtils.nanoTime();
 
@@ -96,15 +106,15 @@ public class MyGdxGame extends ApplicationAdapter {
 		batch.draw(Ground, GroundRectangle.x, GroundRectangle.y);
 		batch.end();
 		if(MainCharacter.x <= 0) MainCharacter.x = 0;
-		if(MainCharacter.x >= 1920 - 64) MainCharacter.x = 1920 - 64;
-		if(MainCharacter.y >= 1080 - 64) MainCharacter.y = 1080 - 64;
+		if(MainCharacter.x >= 1920 - 84) MainCharacter.x = 1920 - 84;
+		if(MainCharacter.y >= 1080 - 256) MainCharacter.y = 1080 - 256;
 		if(Gdx.input.isKeyPressed(Input.Keys.A)) {
 			prevx = MainCharacter.x;
-			MainCharacter.x -= 400 * Gdx.graphics.getDeltaTime();
+			MainCharacter.x -= 650 * Gdx.graphics.getDeltaTime();
 		}
 		if(Gdx.input.isKeyPressed(Input.Keys.D)) {
 			prevx = MainCharacter.x;
-			MainCharacter.x += 400 * Gdx.graphics.getDeltaTime();
+			MainCharacter.x += 650 * Gdx.graphics.getDeltaTime();
 		}
 
 		if (isJumping) { // объект движется только если он прыгает
@@ -118,31 +128,19 @@ public class MyGdxGame extends ApplicationAdapter {
 		}
 
 		if (Gdx.input.isKeyPressed(Input.Keys.SPACE) && !isJumping) {
-			VelocityY = 600; // задаем начальную вертикальную скорость объекта при прыжке
+			VelocityY = 650; // задаем начальную вертикальную скорость объекта при прыжке
 			isJumping = true;
 		}
 		if (MainCharacter.y > 0){
 			isJumping = true;
 		}
 		if (GroundRectangle.overlaps(MainCharacter)){
-			if (MainCharacter.y < GroundRectangle.y + 63) {
-				if (MainCharacter.x > GroundRectangle.x) {
-					MainCharacter.x = GroundRectangle.x + 64;
-				}
-				if (MainCharacter.x < GroundRectangle.x) {
-					MainCharacter.x = GroundRectangle.x - 64;
-				}
-			}
+			MainCharacter.x = prevx;
 			if (MainCharacter.y > GroundRectangle.y){
 				MainCharacter.y = GroundRectangle.y + 64;
 				VelocityY = 0;
 				isJumping = false;
 
-			}
-			else if (MainCharacter.y < GroundRectangle.y) {
-				MainCharacter.y = GroundRectangle.y - 64;
-				VelocityY = -300;
-				isJumping = true;
 			}
 		}
 		for (Iterator<Rectangle> iter = raindrops.iterator(); iter.hasNext(); ) {
@@ -150,14 +148,20 @@ public class MyGdxGame extends ApplicationAdapter {
 			coin.y -= 200 * Gdx.graphics.getDeltaTime();
 			if(coin.y + 64 < 0) iter.remove();
 			if(coin.overlaps(MainCharacter)) {
+				ScoreNum++;
+				YourScoreNum = "score: " + ScoreNum;
 				CoinPick.play();
 				iter.remove();
 			}
 		}
+		batch.begin();
+		font.draw(batch, YourScoreNum, 100, 1000);
+		batch.end();
 	}
 
 	@Override
 	public void dispose () {
+		font.dispose();
 		batch.dispose();
 		Coin.dispose();
 		background.dispose();
